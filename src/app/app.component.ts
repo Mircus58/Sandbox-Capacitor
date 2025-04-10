219f1d3bd46f4d5a4efc76d152b2afb8e1136765
Mircus58 <spagnolo_mg@outlook.com>
2025-04-10 11:48:30 +0200

    petit test appareil photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Capacitor-Sandbox';
+  imgSrc?: string;
+
+
+  async takePicture(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+  })
+  this.imgSrc = image.webPath
+}
 }
